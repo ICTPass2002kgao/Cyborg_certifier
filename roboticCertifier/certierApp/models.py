08dec10403e87763d1527de6bb441a9d117bea89
Kgaogelo <kgaogelojoseph33@gmail.com>
2025-01-11 23:42:29 -0800

    initial commit

---
+++ roboticCertifier/certierApp/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser 
+
+
+class userDocuments(models.Model):
+    pdf=models.FileField(upload_to='pdfs/',verbose_name='Document',blank=True,null=True)
+    image=models.ImageField(upload_to='images/',verbose_name='Document',blank=True,null=True)
+    uploaded_at=models.DateTimeField(auto_now=True,blank=True,null=True)
+    email = models.EmailField(verbose_name="Email",max_length=254,blank=True,null=True)
+    address = models.CharField(max_length = 255, blank=True,null=True)
+    def __str__(self):
+        return self.email 
+class UserFaceVerification(models.Model):
+    id_front_face=models.ImageField(upload_to='image_id/',verbose_name='Document',blank=True,null=True)
+    id_back_face=models.ImageField(upload_to='image_id/',verbose_name='Document',blank=True,null=True)
+    recognised_face=models.ImageField(upload_to='recognised_face/',verbose_name='Document',blank=True,null=True)
+    stamp=models.ImageField(upload_to='Stamp/',verbose_name='Document',blank=True,null=True)
+    uploaded_at=models.DateTimeField(auto_now=True,blank=True,null=True)
+    email = models.EmailField(verbose_name="Email",max_length=254,blank=True,null=True)
+    pdf_path = models.FileField(upload_to='verified_documents/')  
+    
+    def __str__(self):
+        return self.uploaded_at    
+class CertifiedDocumentUpload(models.Model):
+    uploaded_at=models.DateTimeField(auto_now=True,blank=True,null=True)
+    stamp = models.ImageField(upload_to='stamps',verbose_name='Stamb_image',blank=True,null=True)
+    address = models.CharField(max_length = 255, unique= True)
+    email = models.EmailField(verbose_name="Email",max_length=254,blank=True,null=True)
+    def __str__(self):
+        return self.email
+    
+class CustomUser(AbstractUser): 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',   
+        blank=True,
+        help_text=('The groups this user belongs to. A user will get all permissions '
+                   'granted to each of their groups.'),
+        verbose_name=('groups'),
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',   
+        blank=True,
+        help_text=('Specific permissions for this user.'),
+        verbose_name=('user permissions'),
+    )
---
+++ roboticCertifier/certierApp/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('upload_document/',views.upload_file,name='upload_files'),
+    path('completed/',views.done,name='success_url'),
+    path('upload_certified_document/',views.upload_certified_document,name='upload_certified_document'), 
+    path('create_stamp/',views.create_stamp,name='create_stamp'), 
+    path('upload_stamp/',views.upload_stamp,name='upload_stamp'), 
+    path('get_stamp/',views.get_stamp,name='get_stamp'), 
+    path('home/',views.home,name='home'),
+    path('', views.login_user, name='login_user'),
+    path('verify-faces/', views.verifyFace, name='verify-face'),
+    
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
