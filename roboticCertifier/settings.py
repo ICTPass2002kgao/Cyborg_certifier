13c35b4bf4fe9058d1a9d0839dc3b81f418942eb
Kgaogelo <kgaogelojoseph33@gmail.com>
2025-01-15 05:03:03 -0800

    updated the settings file

--- roboticCertifier/settings.py
+++ roboticCertifier/settings.py
@@ -26,7 +26,7 @@ CSRF_TRUSTED_ORIGINS = ['https://cyborgcertifier-production.up.railway.app/']
 SECRET_KEY = 'django-insecure-b#ez@%c%tx2#uu(_b@2e8v8ej8d!9$9(&kaz!51w0en@$l@rh7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.0.242','127.0.0.1','10.0.2.2','cyborgcertifier-production.up.railway.app']
 
